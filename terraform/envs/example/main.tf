f3eabc6f7d726f74a73ac86303b70272893993b7
Donngi <22317839+Donngi@users.noreply.github.com>
2024-03-23 09:51:18 +0900

    Rename directories

--- terraform/envs/example/main.tf
+++ terraform/envs/example/main.tf
@@ -1,16 +1,16 @@
 module "network" {
-  source = "../../module/network"
+  source = "../../modules/network"
 }
 
 module "loadbalancer" {
-  source                 = "../../module/loadbalancer"
+  source                 = "../../modules/loadbalancer"
   subnet_public_a_id     = module.network.subnet_public_a_id
   subnet_public_c_id     = module.network.subnet_public_c_id
   aws_vpc_ecs_fargate_id = module.network.aws_vpc_ecs_fargate_id
 }
 
 module "container" {
-  source                         = "../../module/container"
+  source                         = "../../modules/container"
   subnet_private_a_id            = module.network.subnet_private_a_id
   subnet_private_c_id            = module.network.subnet_private_c_id
   aws_vpc_ecs_fargate_id         = module.network.aws_vpc_ecs_fargate_id
